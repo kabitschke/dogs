53bd2925ddbdf1958149a10d7ebe8055c481de6c
Maycon Kabitschke <mayconkabitschke@gmail.com>
2026-09-01 12:05:30 -0300

    menuMobile

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
 import MinhasFotos from '../../Assets/feed.svg?react';
 import Estatisticas from '../../Assets/estatisticas.svg?react';
@@ -12,32 +12,48 @@ const UserHeaderNav = () => {
   const { userLogout } = React.useContext(UserContext);
   const navigate = useNavigate();
   const mobile = useMedia('(max-width: 40rem)');
-
-
+  const [mobileMenu, setMobileMenu] = React.useState(false);
 
   const handleLogout = () => {
     userLogout();
     navigate('/login');
-  }
+  };
+
+  const { pathname } = useLocation();
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
+
   return (
-    <nav className={styles.nav}>
-      <NavLink to="/conta" end>
-        <MinhasFotos />
-        {mobile && 'Minhas Fotos'}
-      </NavLink>
-      <NavLink to="/conta/estatisticas">
-        <Estatisticas />
-        {mobile && 'Estatísticas'}
-      </NavLink>
-      <NavLink to="/conta/postar">
-        <AdicionarFoto />
-        {mobile && 'Adicionar Foto'}
-      </NavLink>
-      <button onClick={handleLogout}>
-        <Sair />
-        {mobile && 'Sair'}
-      </button>
-    </nav>
+    <>
+      {mobile && (
+        <button
+          aria-label="Menu"
+          onClick={() => setMobileMenu(!mobileMenu)}
+          className={`${styles.mobileButton} ${mobileMenu && styles.mobileButtonActive}`}
+        ></button>
+      )}
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} ${mobileMenu && styles.navMobileActive}`}
+      >
+        <NavLink to="/conta" end>
+          <MinhasFotos />
+          {mobile && 'Minhas Fotos'}
+        </NavLink>
+        <NavLink to="/conta/estatisticas">
+          <Estatisticas />
+          {mobile && 'Estatísticas'}
+        </NavLink>
+        <NavLink to="/conta/postar">
+          <AdicionarFoto />
+          {mobile && 'Adicionar Foto'}
+        </NavLink>
+        <button onClick={handleLogout}>
+          <Sair />
+          {mobile && 'Sair'}
+        </button>
+      </nav>
+    </>
   );
 };
 
--- src/Hooks/useMedia.jsx
+++ src/Hooks/useMedia.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 
 const useMedia = (media) => {
+  const [match, setMatch] = React.useState(null);
 
-    const [match, setMatch] = React.useState(null);
+  React.useEffect(() => {
+    function changeMatch() {
+      const { matches } = window.matchMedia(media);
+      setMatch(matches);
+    }
+    changeMatch();
 
-    React.useEffect(() => {
-        function changeMatch() {
-            const { matches } = window.matchMedia(media);
-            setMatch(matches);
-        }
+    window.addEventListener('resize', changeMatch);
+    return () => {
+      window.removeEventListener('resize', changeMatch);
+    };
+  }, [media]);
 
-        window.addEventListener('resize', changeMatch);
-        return () => {
-            window.removeEventListener('resize', changeMatch)
-        }
-
-    }, [media]);
-
-    return match;
-}
+  return match;
+};
 
 export default useMedia;
