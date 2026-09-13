07e14ff501e3b0f95d1a42bf587f99be6ce3c41f
Maycon Kabitschke <mayconkabitschke@gmail.com>
2026-09-13 17:09:17 -0300

    photo

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Login from './Components/Login/Login';
 import { UserStorage } from './UserContext';
 import User from './Components/User/User';
 import ProtectedRouter from './Components/Helper/ProtectedRouter';
+import Photo from './Components/Photo/Photo';
 
 const App = () => {
   return (
@@ -25,7 +26,7 @@ const App = () => {
                 </ProtectedRouter>
               }
             />
-
+            <Route path="foto/:id" element={<Photo />} />
             {/** O asterico indica que há sub Rotas */}
           </Routes>
           <Footer />
--- src/api.jsx
+++ src/api.jsx
@@ -110,4 +110,3 @@ export function PHOTO_DELETE(id) {
     },
   };
 }
-
